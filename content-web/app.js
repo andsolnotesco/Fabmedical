18787acd66b303889141566de842d93d9dffacee
andsolnotesco <asolonovich@notesco.com>
2022-09-27 15:22:48 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("5bb95b8f-11e2-46be-9f1f-17f016f78552");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
--- perftest.sh
+++ perftest.sh
@@ -1,6 +1,6 @@
-host="fabmedical-[SUFFIX].documents.azure.com"
-username="fabmedical-[SUFFIX]"
-password="[PASSWORD]"
+host="fabmedical-747641.mongo.cosmos.azure.com"
+username="fabmedical-747641"
+password="gkp7odqTyrwsHSkS78tJ5NTBr1ixTK5LOz8OnHKztHgQZUYJwNq9TcCRbiJLmySrT6eBbm4ZUeOOgDlOk2WJSQ=="
 dbname="contentdb"
 port="10255"
 
@@ -10,3 +10,4 @@ for i in {1..250}; do
         --ssl --sslAllowInvalidCertificates \
         --eval 'db.sessions.insert({"title":"Perf Test"})' &
 done
+
